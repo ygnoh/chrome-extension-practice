3301c259fd365f6f8957613670548e95e042fbb7
yonggoonoh <nohyg90@gmail.com>
2018-02-19 22:41:21 +0900

    new issue에 하단 버튼 노출

--- getSource.js
+++ getSource.js
@@ -1,8 +1,25 @@
-function getSource(documentBody) {
-    return documentBody.innerText;
+const labels = document.createElement("li");
+labels.innerText = "HI";
+labels.style.display = "none";
+
+function getSource(target) {
+    target.appendChild(labels);
+    target.addEventListener("click", function (e) {
+        e.preventDefault();
+        toggleLabels();
+    });
+    return "well-done";
+}
+
+function toggleLabels() {
+    if (labels.style.display === "none") {
+        labels.style.display = "block";
+    } else {
+        labels.style.display = "none";
+    }
 }
 
 chrome.extension.sendMessage({
     action: "getSource",
-    source: getSource(document.body)
+    source: getSource(document.querySelector(".btn-primary"))
 });

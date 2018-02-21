f85f5221a91a03b9dcd67ac059f14e85c75c0c2d
yonggoonoh <nohyg90@gmail.com>
2018-02-21 23:35:17 +0900

    TypeError 에러 임시 방어처리

--- content.js
+++ content.js
@@ -1,6 +1,10 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if (msg === "url-updated" || msg === "page-refreshed") {
         const newIssueButton = document.querySelector('a.btn[href$="/issues/new"]')
+        // TODO: onUpdated 이벤트가 페이지가 이동하기 전에 발생하여 생기는 TypeError 임시 방어 처리
+        if (!newIssueButton) {
+            return;
+        }
 
         const plusButton = document.createElement("button");
         plusButton.classList.add("btn", "btn-primary", "select-menu-button", "float-right");
